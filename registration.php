5b446589c8ab9773adca47e0c1ccad8623a98b90
kanidhaya <32258682+kanidhaya@users.noreply.github.com>
2018-12-13 20:39:31 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Test_Exampletest',
+	'test_Exampletest',
 	__DIR__
 );
